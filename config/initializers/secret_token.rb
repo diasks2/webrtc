873458423c4c301542a7e702b01474598d542f61
Phuoc Do <dnprock@gmail.com>
2012-12-22 11:34:18 -0800

    Initialize rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebrtcRails::Application.config.secret_token = 'e81da83cd4957d9633355dcfb3b28eed25e419b39ca1c9fae480d1b28120f496c5453f6763878e1fdce8aa8eff9ea67a4adafd51f87ddd71a3cf3c6c22d25d7d'
