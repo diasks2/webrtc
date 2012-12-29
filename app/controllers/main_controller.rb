792a0341955e5d9cdd5741b9c59515e462bfa300
Phuoc Do <dnprock@gmail.com>
2012-12-28 19:14:01 -0800

    Track online users

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,6 +3,8 @@ class MainController < ApplicationController
     if session[:oauth_access_token] then
       graph = Koala::Facebook::API.new(session[:oauth_access_token])
       @profile = graph.get_object("me")
+      @time = Time.now.to_i
+      @client_id = Digest::SHA1.hexdigest([Time.now, rand].join).encode('UTF-8')
     end
   end
 end
