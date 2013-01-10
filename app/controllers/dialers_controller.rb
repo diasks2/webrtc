ec2944e954a8c405a1d33a5b6ed2299acc0609b8
Phuoc Do <dnprock@gmail.com>
2013-01-10 13:05:48 -0800

    Ask for login in dialer tab

--- app/controllers/dialers_controller.rb
+++ app/controllers/dialers_controller.rb
@@ -1,16 +1,18 @@
 class DialersController < ApplicationController
   def index
-    @client_id = Digest::SHA1.hexdigest([Time.now, rand].join).encode('UTF-8')
-    @time = Time.now.to_i
-    
-    default_client = 'webrtcphone'
-    account_sid = 'AC8299501d55c3b6c62d7b304cc48c9bf7'
-    auth_token = '3c686e6607dd19528396571cd9bf975a'
-    capability = Twilio::Util::Capability.new account_sid, auth_token
-    # Create an application sid at twilio.com/user/account/apps and use it here
-    capability.allow_client_outgoing "AP3e36a472688f5917dc76724025fb1e3d"
-    capability.allow_client_incoming default_client
-    @token = capability.generate
+    if session[:oauth_access_token] then
+      graph = Koala::Facebook::API.new(session[:oauth_access_token])
+      @profile = graph.get_object("me")
+      
+      default_client = 'webrtcphone'
+      account_sid = 'AC8299501d55c3b6c62d7b304cc48c9bf7'
+      auth_token = '3c686e6607dd19528396571cd9bf975a'
+      capability = Twilio::Util::Capability.new account_sid, auth_token
+      # Create an application sid at twilio.com/user/account/apps and use it here
+      capability.allow_client_outgoing "AP3e36a472688f5917dc76724025fb1e3d"
+      capability.allow_client_incoming default_client
+      @token = capability.generate
+    end
   end
   
   def voice
